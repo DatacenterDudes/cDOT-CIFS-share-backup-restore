717ec665390aa1259437d14d454d3776d7a0ec35
lance36 <lance36@gmail.com>
2016-02-23 12:01:39 +0100

    Added files via upload

---
+++ mountAllVserverVolumes.ps1
@@ -0,0 +1,21 @@
+﻿Param([parameter(Mandatory = $true)] [alias("s")] $server,
+      [parameter(Mandatory = $true)] [alias("u")] $user,
+      [parameter(Mandatory = $true)] [alias("p")] $password,
+      [parameter(Mandatory = $true)] [alias("v")] $vserver)
+
+Import-Module "C:\Program Files (x86)\Netapp\Data ONTAP PowerShell Toolkit\DataONTAP"
+
+$passwd = ConvertTo-SecureString $password -AsPlainText -Force
+$cred = New-Object -typename System.Management.Automation.PSCredential -ArgumentList $user, $passwd
+$nctlr = Connect-NcController $server -Credential $cred
+$nodesinfo = @{}
+
+
+get-ncVol -Vserver $vserver| foreach { 
+
+   $myVol = $_.Name
+     if (($myVol -ne $null) -and ($myVol -ne "") -and ($myVol -notlike "*root*") )
+    {
+         Mount-NcVol -vservercontext  svm_nfs_02 -name $myVol -JunctionPath "/$myVol"
+    }
+}
